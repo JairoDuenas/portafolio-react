640d3a77e0eca7dda7cbe8d21a0857426fa71120
Jairo Dueñas <jairoduenas.ing@gmail.com>
2026-04-08 10:35:37 -0500

    Cambio link de

--- src/constants/data.js
+++ src/constants/data.js
@@ -15,7 +15,7 @@ export const PROJECTS = [
         title: "Ver código en GitHub",
       },
       {
-        href: "https://control-gastos-react-vite-one.vercel.app/login",
+        href: "https://gasto-smart-two.vercel.app/login",
         icon: vercelIcon,
         title: "Ver proyecto en Vercel",
       },
